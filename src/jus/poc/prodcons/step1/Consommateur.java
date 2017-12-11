10bbd6da7f523fb1d54c4e81a721eccddff97dfc
ProfesseurSid <salutlesmanchots@hotmail.fr>
2017-12-11 16:57:33 +0100

    messageX.toString tient compte du temps de put Consommateur consomme des messages sans mettre à jour ses stats TestProdCons implémente le tampon

--- src/jus/poc/prodcons/step1/Consommateur.java
+++ src/jus/poc/prodcons/step1/Consommateur.java
@@ -6,20 +6,27 @@ import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons._Consommateur;
 
 public class Consommateur extends Acteur implements _Consommateur {
-
+	int nbMsgLus = 0;
+	
 	protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement,
 			int deviationTempsDeTraitement) throws ControlException {
 		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		this.setDaemon(true);
 	}
 
 	@Override
 	public int nombreDeMessages() {
-		// TODO Auto-generated method stub
-		return 0;
+		return nbMsgLus;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stubb
+		while(true) {
+			try {
+				TestProdCons.tampon.get(this);
+			} catch(Exception e) {
+				System.out.println(e.toString());
+			}
+		}
 	}
 }
