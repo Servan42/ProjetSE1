04b635418f186cfce360321b240c5b5307c658a3
Servan42 <charlot.servan@gmail.com>
2017-12-11 17:13:53 +0100

    Prodcons get implemented

--- src/jus/poc/prodcons/step1/ProdCons.java
+++ src/jus/poc/prodcons/step1/ProdCons.java
@@ -27,18 +27,41 @@ public class ProdCons implements Tampon {
 		buffer = new MessageX[buffSize];
 	}
 
+	
+	/**
+	 * @return Nombre de message en attente de consommation (nombre de cases utilisées dans le tableau) 
+	 */
 	@Override
 	public int enAttente() {
-		return buffer.length;
+		int messages = 0;
+		for (int i = 0; i < buffSize; i++) {
+			if(buffer[i] != null) messages++;
+		}
+		return messages;
 	}
 
 	@Override
 	public synchronized Message get(_Consommateur arg0) throws Exception, InterruptedException {
-		while(!(buffer.length > 0)){
-			wait();
+		long maxTime = 0;
+		int maxId = 0;
+		MessageX retour;
+		while (!(enAttente() > 0)) {
+			try {
+				wait();
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
 		}
-		buffer.
-		return null;
+		for(int i = 0; i < buffSize; i++){
+			if(buffer[i].getTime() > maxTime){
+				maxTime = buffer[i].getTime();
+				maxId = i;
+			}
+		}
+		retour = buffer[maxId];
+		buffer[maxId] = null;
+		notifyAll();
+		return retour;
 	}
 
 	@Override
