c61694f829bf6de80952b4ded30718807e6b2172
ProfesseurSid <salutlesmanchots@hotmail.fr>
2017-12-11 15:23:51 +0100

    creation de ProdCons

--- src/jus/poc/prodcons/step1/ProdCons.java
+++ src/jus/poc/prodcons/step1/ProdCons.java
@@ -1,35 +1,41 @@
 package jus.poc.prodcons.step1;
 
+import java.util.ArrayList;
+
 import jus.poc.prodcons.Message;
 import jus.poc.prodcons.Tampon;
 import jus.poc.prodcons._Consommateur;
 import jus.poc.prodcons._Producteur;
 
 public class ProdCons implements Tampon {
-
+	int buffSize;
+	MessageX[] buffer;
+	
+	public ProdCons(int size){
+		buffSize = size;
+		buffer = new MessageX[buffSize];
+	}
+	
 	@Override
 	public int enAttente() {
-		// TODO Auto-generated method stub
-		return 0;
+		return buffer.length;
 	}
 
 	@Override
 	public Message get(_Consommateur arg0) throws Exception, InterruptedException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void put(_Producteur arg0, Message arg1) throws Exception, InterruptedException {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public int taille() {
-		// TODO Auto-generated method stub
-		
-		return 0;
+		return buffSize;
 	}
 
 }
